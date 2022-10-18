d3e83cea57112061fef00b8f6414c00d033d8194
Richard Wolff <rwolff@g.ucla.edu>
2022-10-18 13:18:16 -0700

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+base_output_dir = "/u/project/ngarud/Garud_lab/metagenomic_fastq_files/Evolution_Experiment"
+base_snps_dir = f"{base_output_dir}/midas_output/merged_midas_output/snps/Lactobacillus"
+base_genes_dir = f"{base_output_dir}/midas_output/merged_midas_output/genes/Lactobacillus"
+
+rep3 = ['WF_parent','Rep3_Pass5','Rep3_Pass9','Rep3_Pass16','Rep3_Pass23','Rep3_Pass30','Rep3_Pass37','Rep3_Pass44','Rep3_Pass51']
+
