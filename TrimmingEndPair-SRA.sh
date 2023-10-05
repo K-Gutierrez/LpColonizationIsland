49b94d07dd8c3b89f9122cb0ce962d6c9caf4823
Karina-GutierrezGarcia <45019299+K-Gutierrez@users.noreply.github.com>
2023-10-05 11:13:44 -0400

    Update TrimmingEndPair-SRA.sh

--- TrimmingEndPair-SRA.sh
+++ TrimmingEndPair-SRA.sh
@@ -3,5 +3,5 @@
 
 for i in $(ls My/path/*_1.fastq); 
 do 
-trim_galore -q 25  -o Trimmed --path_to_cutadapt /mnt/sequence/linux_3/trimgalore/env/bin/cutadapt --paired --clip_R1 15 --clip_R2 15 --fastqc --dont_gzip $i ${i/_1.fastq/_2.fastq};
+trim_galore -q 25  -o Trimmed --path_to_cutadapt /My/path/trimgalore/env/bin/cutadapt --paired --clip_R1 15 --clip_R2 15 --fastqc --dont_gzip $i ${i/_1.fastq/_2.fastq};
 done
