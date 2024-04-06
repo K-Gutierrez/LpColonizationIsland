5e50f3a961e3944a69888c046ea878fb8877f7c0
Richard Wolff <rwolff@g.ucla.edu>
2024-04-05 17:56:02 -0700

    copy number dynamics

--- config.py
+++ config.py
@@ -1,6 +1,19 @@
-base_output_dir = "/u/project/ngarud/Garud_lab/metagenomic_fastq_files/Evolution_Experiment"
+base_input_dir = "/u/project/ngarud/Garud_lab/metagenomic_fastq_files/Evolution_Experiment/"
+base_output_dir = "/u/project/ngarud/rwolff/Evolution_Experiment"
+midas_dir = "/u/project/ngarud/rwolff/Evolution_Experiment/midas_db"
+
+
 base_snps_dir = f"{base_output_dir}/midas_output/merged_midas_output/snps/Lactobacillus"
 base_genes_dir = f"{base_output_dir}/midas_output/merged_midas_output/genes/Lactobacillus"
 
-rep3 = ['WF_parent','Rep3_Pass5','Rep3_Pass9','Rep3_Pass16','Rep3_Pass23','Rep3_Pass30','Rep3_Pass37','Rep3_Pass44','Rep3_Pass51']
 
+rep3 = ['WF_parent','Rep3_Pass5','Rep3_Pass9','Rep3_Pass16','Rep3_Pass23','Rep3_Pass30','Rep3_Pass37','Rep3_Pass44','Rep3_Pass51']
+rep3_dic = {'WF_parent':"Parent",
+        'Rep3_Pass5':"Passage 5",
+        'Rep3_Pass9': "Passage 9",
+        'Rep3_Pass16':"Passage 16",
+        'Rep3_Pass23':"Passage 23",
+        'Rep3_Pass30':"Passage 30",
+        'Rep3_Pass37':"Passage 37",
+        'Rep3_Pass44':"Passage 44",
+        'Rep3_Pass51':"Passage 51"}
